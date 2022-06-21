9ffa75a900da3471834925fe68080ba57bb9e65b
khushi Gupta <95909861+khushi89012@users.noreply.github.com>
2022-06-21 23:16:24 +0530

    deleted

--- task2/src/Redux/action.js
+++
@@ -1,8 +0,0 @@
-
-export const AddItem=(data)=>{
-    return{
-        type:'ADD_ITEM',
-        payload:data
-    }
-}
-
--- task2/src/Redux/store.js
+++
@@ -1,6 +0,0 @@
-import {legacy_createStore} from 'redux';
-
-import { AddReducer } from './reducers';
-
-
-export const store = legacy_createStore(AddReducer);
